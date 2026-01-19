03a62d8a969c8ebf0ededa92c934b2c589eacc54
sneha0099 <snehaprajapati678@gmail.com>
2026-01-19 14:27:10 +0530

    session 1

--- session1/index.js
+++ session1/index.js
@@ -1,1 +1,36 @@
- 
+console.log(`Value of a before declaration is ${a}`);
+var a;
+console.log(`a after declaration is ${a}`);
+
+a = 10;
+
+console.log(`Value of a after declaration is ${a}`);
+
+
+//console.log(`Value of b before declaration is ${b}`);
+let b;
+console.log(`b after declaration is ${b}`);
+
+b = 20;
+
+console.log(`Value of b after declaration is ${b}`);
+
+greeting();
+console.log(greeting);
+
+function greeting(){
+    console.log("Hello, World!");
+}
+
+var greet2 = function(){
+    console.log("Hello, Worldd!");
+}
+
+greet2();
+
+console.log(greet2);
+
+//----------
+
+console.log(s);
+var s = 1;
